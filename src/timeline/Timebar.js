099d6b3619841168e6dacaa701df140b95518b9e
João Vargas <joao.gaiares@gmail.com>
2025-08-11 10:44:09 -0300

    refactor: created separated component for Timebar

---
+++ src/timeline/Timebar.js
@@ -0,0 +1,29 @@
+const Timebar = ({item, laneIndex, laneHeight, left, width, minItemWidth}) => {
+  
+  return (
+    <div
+      key={item.id}
+      style={{
+        position: "absolute",
+        top: 120 + laneIndex * laneHeight,
+        left: left,
+        width: Math.max(width, minItemWidth),
+        height: laneHeight - 10,
+        background: "#4e79a7",
+        color: "white",
+        borderRadius: 8,
+        padding: '8px',
+        boxSizing: 'border-box',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        whiteSpace: "nowrap",
+        fontSize: 12,
+      }}
+      title={`${item.name} (${item.start} → ${item.end})`}
+    >
+      {item.name}
+    </div>
+  )
+}
+
+export default Timebar;
--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,6 +1,7 @@
 import assignLanes from "../assignLanes"
 import timelineItems from "../timelineItems"
 import { dateToX, calculateWidthPerDays } from "../utils/daysUtils";
+import Timebar from "./Timebar";
 
 export default function Timeline() {
   const lanes = assignLanes(timelineItems);
@@ -19,29 +20,16 @@ export default function Timeline() {
       {lanes.map((lane, laneIndex) =>
         lane.map((item) => {
           const width = calculateWidthPerDays(item.end, item.start, 20)
+          
           return (
-            <div
-              key={item.id}
-              style={{
-                position: "absolute",
-                top: 120 + laneIndex * laneHeight,
-                left: dateToX(item.start, minDate),
-                width: Math.max(width, minItemWidth),
-                height: laneHeight - 10,
-                background: "#4e79a7",
-                color: "white",
-                borderRadius: 8,
-                padding: '8px',
-                boxSizing: 'border-box',
-                textOverflow: 'ellipsis',
-                overflow: 'hidden',
-                whiteSpace: "nowrap",
-                fontSize: 12,
-              }}
-              title={`${item.name} (${item.start} → ${item.end})`}
-            >
-              {item.name}
-            </div>
+            <Timebar 
+              item={item} 
+              laneIndex={laneIndex}
+              laneHeight={laneHeight}
+              width={width}
+              minItemWidth={minItemWidth}
+              left={dateToX(item.start, minDate)}
+            />
           )
         }
         )
